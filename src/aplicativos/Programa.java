3082593bc46916b1b5231fbcbddf1229563147dc
victojoao <victojoao789@gmail.comgit config --global user.email victojoao789@gmail.com[200~git config --global user.email~git config --global user.email victojoao789@gmail.com>
2022-12-01 18:49:31 -0300

    first class posirion

--- src/aplicativos/Programa.java
+++ src/aplicativos/Programa.java
@@ -1,11 +1,12 @@
 package aplicativos;
 
-
+import boardgame.Position;
 
 public class Programa {
 
 	public static void main(String[] args) {
-
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 		
 
 	}
