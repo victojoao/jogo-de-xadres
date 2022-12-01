fb5631e35e4e2fdc1c77ac18c657e93eaed951d1
victojoao <victojoao789@gmail.comgit config --global user.email victojoao789@gmail.com[200~git config --global user.email~git config --global user.email victojoao789@gmail.com>
2022-12-01 19:45:18 -0300

    Chess layer and printing the board

--- src/aplicativos/Programa.java
+++ src/aplicativos/Programa.java
@@ -1,11 +1,13 @@
 package aplicativos;
 
-import boardgame.Board;
+import chess.ChessMatch;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		Board board =new Board (8,8);
+		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 		
 		
 
