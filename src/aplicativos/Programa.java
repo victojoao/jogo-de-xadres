786542fd59218186bb8f0a1f01d1bfaba52fd8eb
victojoao <victojoao789@gmail.comgit config --global user.email victojoao789@gmail.com[200~git config --global user.email~git config --global user.email victojoao789@gmail.com>
2022-12-01 18:43:45 -0300

    acerto

--- src/aplicativos/Programa.java
+++ src/aplicativos/Programa.java
@@ -1,13 +1,12 @@
 package aplicativos;
 
-import tabuleiro.Posições;
-import tabuleiro.tabuleiro;
+
 
 public class Programa {
 
 	public static void main(String[] args) {
 
-		tabuleiro tabaleiro = new tabuleiro(8, 8);		
+		
 
 	}
 
