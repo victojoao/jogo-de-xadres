fd11958ee46583aac7aee6087315bf701825fc3b
victojoao <victojoao789@gmail.comgit config --global user.email victojoao789@gmail.com[200~git config --global user.email~git config --global user.email victojoao789@gmail.com>
2022-12-01 19:09:20 -0300

    Start to implement Board and Piece

--- src/aplicativos/Programa.java
+++ src/aplicativos/Programa.java
@@ -1,12 +1,12 @@
 package aplicativos;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+		Board board =new Board (8,8);
+		
 		
 
 	}
