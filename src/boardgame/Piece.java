a6b8e837ee37b08d4cf9f30edcd10a7d0fb8608d
victojoao <victojoao789@gmail.comgit config --global user.email victojoao789@gmail.com[200~git config --global user.email~git config --global user.email victojoao789@gmail.com>
2022-12-05 19:17:01 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	protected Position position;
 	private Board board;
 	public Piece(Board board) {
@@ -10,7 +10,21 @@ public class Piece {
 	protected Board getBoard() {
 		return board;
 	}
-
 	
-
+	public abstract boolean [][] possibleMoves();
+	
+	public boolean posssibleMoves (Position position) {
+		return possibleMoves()[position.getRow()][position.getColunm()];
+	}
+	public boolean isThearAnyPossibleMove() {
+		boolean [][] mat = possibleMoves();
+		for(int i =0; i<mat.length; i++) {
+			for(int j=0; j<mat.length;j++) {
+				if(mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
