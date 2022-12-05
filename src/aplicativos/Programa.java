883bb669aff6442ec03020bc9e34c487c26cad9a
victojoao <victojoao789@gmail.comgit config --global user.email victojoao789@gmail.com[200~git config --global user.email~git config --global user.email victojoao789@gmail.com>
2022-12-05 18:53:00 -0300

    Handling exeptions and clering screen

--- src/aplicativos/Programa.java
+++ src/aplicativos/Programa.java
@@ -1,12 +1,14 @@
 package aplicativos;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
-public class Programa {
+public class Programa { 
 
 	public static void main(String[] args) {
 		
@@ -14,17 +16,28 @@ public class Programa {
 		ChessMatch chessMatch = new ChessMatch();
 		
 		while (true) {
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.print("Source: ");
-			ChessPosition source = UI.readChessPosition(sc);
-			
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			try {     
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			}
+			catch (ChessException e ) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e ) {
+				System.out.println(e.getMessage());
+				sc.nextLine(); 
 		}
 		
 	}
-} 
+}
+}
